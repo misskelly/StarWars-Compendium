691afa74ef471ae243af2d4b8d9eb2e9dc1242ef
Kelly Zick <kllyzck@gmail.com>
2019-05-01 18:27:26 -0600

    Film fetched and cleaned, but still living in App

--- swapi/src/App.js
+++ swapi/src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { fetchFilm } from './helpers/fetchCalls.js'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      people: [],
+      planets: [],
+      vehicles: [],
+      film: {}
+    }
+  }
+
+  fetchFilm = () => {
+    const randomNum = Math.floor(Math.random() * (7 - 1) + 1);
+    const url = `https://www.swapi.co/api/films/${randomNum}`
+    return fetch(url)
+      .then(response => response.json())
+      .then(film => this.cleanFilm(film))
+      .catch(error => console.log(error))
+  }
+
+  cleanFilm = film => {
+    const { title, opening_crawl, release_date } = film
+    const clean = { 
+      title: title,
+      scroll: opening_crawl,
+      year: release_date.slice(0, 4)
+    }
+    this.setState({film: clean})
+  }
+
+  componentDidMount() {
+    this.fetchFilm()
+  }
+  
+  render() {
+
+    return (
+      <div className="App">
       <h1>StarWars</h1>
     </div>
   );
 }
 
-export default App;
+}
+
---
+++ swapi/src/helpers/cleaners.js
@@ -0,0 +1,9 @@
+// export const cleanFilm = film => {
+//   const { title, opening_crawl, release_date } = film
+//   const clean = { 
+//     title: title,
+//     scroll: opening_crawl,
+//     year: release_date.slice(0, 4)
+//   }
+//   return clean
+// }
