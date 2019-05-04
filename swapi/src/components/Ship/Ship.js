908b5801d6089139374749f01b716668c5e89fe6
Kelly Zick <kllyzck@gmail.com>
2019-05-04 13:17:58 -0600

    Add ship structure

--- swapi/src/components/Ship/Ship.js
+++ swapi/src/components/Ship/Ship.js
@@ -3,6 +3,30 @@ import React from 'react'
 const Ship = () => {
   return (
     <section className='ship-container'>
+    <div id="wheel" class="pulse spinWheel">
+            <div id="inner">
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div class="segment teal"><span> 0 0 0 </span></div>
+              <div class="segment blue"><span> 0 0 0 </span></div>
+            </div>
+          </div>
     </section>
   )
 }
