bda5d7d66bd014873ebca9fcca4113aa7f080ff1
Kelly Zick <kllyzck@gmail.com>
2019-05-04 13:51:17 -0600

    Add ship animation

--- swapi/src/components/Ship/Ship.js
+++ swapi/src/components/Ship/Ship.js
@@ -3,28 +3,28 @@ import React from 'react'
 const Ship = () => {
   return (
     <section className='ship-container'>
-    <div id="wheel" class="pulse spinWheel">
+    <div id="ship" className="pulse spinWheel">
             <div id="inner">
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
-              <div class="segment teal"><span> 0 0 0 </span></div>
-              <div class="segment blue"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
+              <div className="segment dash"><span> 0 0 0 </span></div>
             </div>
           </div>
     </section>
