ae70c708e24e11f0f51ab4777c2d7bdfe36a7eef
Kelly Zick <kllyzck@gmail.com>
2019-05-03 20:15:09 -0600

    Crawl beaten into submission

--- swapi/src/components/FeaturedFilm/FeaturedFilm.js
+++ swapi/src/components/FeaturedFilm/FeaturedFilm.js
@@ -5,13 +5,11 @@ import 'react-star-wars-crawl/lib/index.css';
 const FeaturedFilm = ({film}) => {
   return (
     <section className='film-container'>
-      <article className='scroll-container'>
       <Crawl
         title={film.title}
         subTitle={film.subtitle}
         text={film.scroll}
       />
-      </article>
       <article className='film-info'>
         <h3 className='film-title'>{film.title}</h3>
         <h4 className='subtitler'>{film.subtitle}</h4>
